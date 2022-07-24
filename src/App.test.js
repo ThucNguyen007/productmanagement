9c7d61963fadd2b11a0d937f757a6d20afa14f3a
Thuc Nguyen <thucdlnguyen@gmail.com>
2022-07-24 11:00:53 -0400

    add jest dependencies supporting testing.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import TestRenderer from 'react-test-renderer';
+import AddProduct from './components/AddProduct';
 import App from './App';
 
-test('renders learn react link', () => {
+/*test('open add car modal form', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  fireEvent.click(screen.getByText('Product Management'));
+});*/
+
+test('renders a snapshot', () => {
+  const tree = TestRenderer.create
+  (<AddProduct/>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
